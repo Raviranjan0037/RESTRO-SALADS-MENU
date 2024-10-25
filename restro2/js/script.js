a3ca2c9ae6d3e9fa84eb0ea13be3979f25adf07d
Ravi Ranjan Kumar <raviranjankumar2627a@gmail.com>
2024-10-25 20:12:22 +0530

    Add files via upload

---
+++ restro2/js/script.js
@@ -0,0 +1,24 @@
+var tl = gsap.timeline();
+
+tl.to(".img_r", {
+
+    rotate: 360,
+    duration: 30,
+    repeat: -1,
+    stagger: 0.3
+
+})
+
+
+// window.onscroll = function () {
+//     var header = document.getElementById('navbar');
+//     if (window.scrollY > 50) {
+//         header.classList.add('scroll');
+//     } else {
+//         header.classList.remove('scroll');
+//     }
+// };
+
+
+
+ 
